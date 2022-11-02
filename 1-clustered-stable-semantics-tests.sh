ade2c60b52dc54e2a88cb1e50e044634b27e5e27
Rafael Bankosegger <rafael@bankosegger.at>
2022-11-02 17:01:09 +0100

    Separate concrete AF and abstract AF-mapping in stable model tests

--- 1-clustered-stable-semantics-tests.sh
+++ 1-clustered-stable-semantics-tests.sh
@@ -1,37 +1,37 @@
 echo "Test 1"
 echo "Classical stable semantics:"
-clingo --verbose=0 tests/cstable-test1.lp semantics/sem-stable.lp 0
+clingo --verbose=0 tests/cstable-1.lp semantics/sem-stable.lp 0
 echo "---"
 echo "Clustered stable semantics:"
-clingo --verbose=0 tests/cstable-test1.lp semantics/clusem-stable.lp 0
+clingo --verbose=0 tests/cstable-1.lp tests/cstable-1-map.lp to-clustered-af.lp semantics/clusem-stable.lp 0
 echo "---"
 
 echo ""
 
 echo "Test 2"
 echo "Classical stable semantics:"
-clingo --verbose=0 tests/cstable-test2.lp semantics/sem-stable.lp 0
+clingo --verbose=0 tests/cstable-2.lp semantics/sem-stable.lp 0
 echo "---"
 echo "Clustered stable semantics:"
-clingo --verbose=0 tests/cstable-test2.lp semantics/clusem-stable.lp 0
+clingo --verbose=0 tests/cstable-2.lp tests/cstable-2-map.lp to-clustered-af.lp semantics/clusem-stable.lp 0
 echo "---"
 
 echo ""
 
 echo "Test 3"
 echo "Classical stable semantics:"
-clingo --verbose=0 tests/cstable-test3.lp semantics/sem-stable.lp 0
+clingo --verbose=0 tests/cstable-3.lp semantics/sem-stable.lp 0
 echo "---"
 echo "Clustered stable semantics:"
-clingo --verbose=0 tests/cstable-test3.lp semantics/clusem-stable.lp 0
+clingo --verbose=0 tests/cstable-3.lp tests/cstable-3-map.lp to-clustered-af.lp semantics/clusem-stable.lp 0
 echo "---"
 
 echo ""
 
 echo "Test 4"
 echo "Classical stable semantics:"
-clingo --verbose=0 tests/cstable-test4.lp semantics/sem-stable.lp 0
+clingo --verbose=0 tests/cstable-4.lp semantics/sem-stable.lp 0
 echo "---"
 echo "Clustered stable semantics:"
-clingo --verbose=0 tests/cstable-test4.lp semantics/clusem-stable.lp 0
+clingo --verbose=0 tests/cstable-4.lp tests/cstable-4-map.lp to-clustered-af.lp semantics/clusem-stable.lp 0
 echo "---"
